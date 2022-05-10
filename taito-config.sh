c77f7e7a5c8c2f84f391039543209142ce1ed198
keskiju <jukka.keskiluopa@taitounited.fi>
2022-05-10 17:12:56 +0300

    chore: add taito-config.sh

---
+++ taito-config.sh
@@ -0,0 +1,7 @@
+taito_environments="dev prod"
+taito_vc_repository_url="github.com/TaitoUnited/imaginary"
+
+link_urls="
+  * builds=https://$taito_vc_repository_url/actions Builds
+  * git=https://$taito_vc_repository_url Git repository
+"
